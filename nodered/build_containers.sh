8cfb514e42701d3069bc9d9d3f1608ae890301cd
Rich Moore <rmoorewrs@gmail.com>
2026-03-31 09:22:59 -0400

    added custom nodered container option

---
+++ nodered/build_containers.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo Building custom nodered container
+export CUR_DIR=$(pwd)
+
+docker build -f Dockerfile.nodered -t mynodered:latest .
+cd ..
+echo building virtual_watertank containers
+docker build -f Dockerfile.watertank -t watertank:latest .
+docker build -f Dockerfile.levelcontroller -t levelcontroller:latest .
+cd ${CUR_DIR}
+
+echo run: 
+echo docker compose -f docker-compose.nodered up 
+echo     -or- 
+echo docker-compose -f docker-compose.nodered up
